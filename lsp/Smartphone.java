4f658dabcb27c68743b5c1dfd2b031dbcce17855
Muhamad Salman Ardiyansyah <msalman21@if.unjani.ac.id>
2023-10-18 18:41:03 +0700

    perubahan sesuai prinsip liscov subs

--- lsp/Smartphone.java
+++ lsp/Smartphone.java
@@ -1,14 +1,8 @@
 package lsp;
 
-import java.sql.Date;
-
 public class Smartphone extends Product {
     @Override
     String getName(){
         return "Samsung S22 Ultra";
-    }
-    @Override
-    Date getExpiredDate(){
-        return new Date(0);
     }
 }
