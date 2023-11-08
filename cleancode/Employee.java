b631bb8660d80a59617bc3cc02402517725b500f
Muhamad Salman Ardiyansyah <msalman21@if.unjani.ac.id>
2023-11-08 11:04:43 +0700

    membuat cleancode 2

---
+++ cleancode/Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+    class Employee{
+        int payAmount() {
+            switch (getType()) {
+                case EmployeeType.ENGINEER:
+                    return _monthlySalary;
+                case EmployeeType.SALESMAN:
+                    return _monthlySalary + _commission;
+                case EmployeeType.MANAGER:
+                    return _monthlySalary + _bonus;
+                default:
+                    throw new Exception("Incorrect Employee");
+            }
+        }
+    }
+}
