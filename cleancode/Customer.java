289484b644247506af84f0a0b767cb6c9f9aba69
Muhamad Salman Ardiyansyah <msalman21@if.unjani.ac.id>
2023-11-08 10:59:49 +0700

    perubahan cleancode1

--- cleancode/Customer.java
+++ cleancode/Customer.java
@@ -1,7 +1,7 @@
 public class Customer {
-    class DtaRcrd102 {
-        private Date genymdhms;
-        private Date modymdhms;
-        private final String pszqint = "102";
+    class Customer {
+        private Date generationTimestamp;
+        private Date modificationTimestamp;
+        private final String recordId = "102";
     }
 }
