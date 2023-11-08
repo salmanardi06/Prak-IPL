8b072d48806bc776fe277aeb4d27492a3da16a70
Muhamad Salman Ardiyansyah <msalman21@if.unjani.ac.id>
2023-11-08 11:11:17 +0700

    perubahan cleancode 3

--- cleancode/Point.java
+++ cleancode/Point.java
@@ -1,6 +1,10 @@
 public class Point {
-    public class Point {
-        public double x;
-        public double y;
+    public interface Point {
+        double getX();
+        double getY();
+        void setCartesian(double x, double y);
+        double getR();
+        double getTheta();
+        void setPolar(double r, double theta);
     }
 }
