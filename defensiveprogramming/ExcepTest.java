100fa8fdb5b9e311a6fc0027d7d1deb0d5d04854
Muhamad Salman Ardiyansyah <msalman21@if.unjani.ac.id>
2023-11-08 16:46:29 +0700

    perubahan exceptest

--- defensiveprogramming/ExcepTest.java
+++ defensiveprogramming/ExcepTest.java
@@ -2,12 +2,14 @@ import java.io.*;
 
 public class ExcepTest {
     public static void main(String args[]) {
-        try {
-        int a[] = new int[2];
-        System.out.println("Access element three :" + a[3]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-        System.out.println("Exception thrown :" + e);
-        }
-        System.out.println("Out of the block");
+    try {
+        file = new FileInputStream(fileName);
+        x = (byte) file.read();
+        } catch (IOException i) {
+        i.printStackTrace();
+        return -1;
+        } catch (FileNotFoundException f) // Not valid! {
+        f.printStackTrace();
+        return -1;
     }
 }
