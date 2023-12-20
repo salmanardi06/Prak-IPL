0bc18ef3ff848615871613add4be384c81d5ed9a
Muhamad Salman Ardiyansyah <msalman21@if.unjani.ac.id>
2023-12-20 19:13:33 +0700

    Conditional expression

---
+++ IPL Modul 9/Conditional.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Hp
+ */
+public class Conditional {
+    public static void main(String[] args) {
+    if ((score <= 700) && ((income < 40000) || (income > 100000) || !authorized|| (score <= 500)) && (income <= 100000))
+    reject()
+    else
+    accept();
+    }
+}
